f3d622f7e40ca7e9bcd0964da25295b68dccab40
Valleon <val.ekechukwu@gmail.com>
2022-09-07 14:32:45 +0100

    Gave functionality to the SAVE and Cancel button

--- src/main/java/com/valentine/users/UserService.java
+++ src/main/java/com/valentine/users/UserService.java
@@ -7,9 +7,14 @@ import java.util.List;
 
 @Service
 public class UserService {
-    @Autowired private UserRepository repo;
+    @Autowired
+    private UserRepository repo;
 
-    public List<User> listAll(){
+    public List<User> listAll() {
         return (List<User>) repo.findAll();
+    }
+
+    public void save(User user) {
+        repo.save(user);
     }
 }
